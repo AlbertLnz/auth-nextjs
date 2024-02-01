6d04c88996b68f4efc777509b43dc4923244e4c2
AlbertLnz <albert2000_lanza@hotmail.com>
2024-02-01 14:52:53 +0100

    NavBar component in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import Navbar from "./components/NavBar";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -14,6 +15,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
+        <Navbar />
         {children}
       </body>
     </html>
