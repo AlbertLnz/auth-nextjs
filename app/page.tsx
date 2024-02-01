1fe056774fa955ef1924cb7075de5efd89e41e58
AlbertLnz <albert2000_lanza@hotmail.com>
2024-02-01 15:20:10 +0100

    Other way to middleware a page, using a session inside the page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,17 @@
-export default function Home() {
-  return (
-    <main>
+import { options } from "./api/auth/[...nextauth]/options"
+import { getServerSession } from "next-auth/next"
 
-    </main>
-  );
+export default async function Home() {
+
+  const session = await getServerSession(options)
+
+  return (
+    <>
+      {session ? (
+        <h1 className="text-2xl mt-6 ml-4">Hi User, welcome!!</h1>
+      ) : (
+        <h1 className="text-2xl mt-6 ml-4">You Shall Not Pass!</h1>
+      )}
+    </>
+  )
 }
